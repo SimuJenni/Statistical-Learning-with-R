ac7a1a8fb730d7f1f5a9f481ef0e0950cc43663b
Simon Jenni <simujenni@students.unibe.ch>
2016-03-13 15:13:35 +0100

    Coding for ex3 done

--- Ex3/ex3_jenni.R
+++ Ex3/ex3_jenni.R
@@ -4,7 +4,7 @@
 setwd("~/Dropbox/Study/Statistical Learning/Exercises/Ex2")
 original_data <- read.table("Mean20.txt", header = T)
 # We remove outliers as in ex2
-my_data = original_data[which(original_data$time > 0),]
+my_data = original_data[which(original_data$time > 0), ]
 
 ## Ex3.1
 secondMax <- function(x) {
@@ -18,7 +18,7 @@ secondMax <- function(x) {
 
 ## Test secondMax
 stopifnot(secondMax(c(1, 9, 3, 6)) == 6)
-stopifnot(secondMax(c(-1,-9, 3, 6)) == 3)
+stopifnot(secondMax(c(-1, -9, 3, 6)) == 3)
 stopifnot(secondMax(c("hello", "salut", "ciao", "hallo")) == "hello")
 stopifnot(secondMax(my_data) == 7.12)
 res = try(secondMax(original_data))
@@ -52,5 +52,26 @@ stopifnot(mySummary(my_data) == c(
 ))
 
 ## Ex3.3
+mu = 2
+std = 1.5
+numSamples = 20
+numVals = 20
 
+# Generate a matrix containing all the samples (row~sample)
+mat = matrix(rnorm(numSamples * numVals, mean = mu, sd = std),
+             nrow = numSamples,
+             ncol = numVals)
 
+# Compute t-test for all the samples (each row)
+res = apply(mat, 1, function(x)
+  t.test(x, mu = 1.5))
+
+# Extract p-values
+pVals = lapply(res, function(x)
+  x$p.value) #extract p-values
+
+# Count the number of correct results (true mean != 1.5)
+numRight = sum(pVals < 0.05)
+
+# Compute t-test on all the sample-values
+t.test(as.vector(mat), mu = 1.5)
